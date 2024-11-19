402f458b2368b7ed55f683c01bced1624df917fc
Bektur Muratov <muratov_b@au.kg>
2024-11-19 10:50:17 +0600

    Changed ProductCard .css and .js

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -5,12 +5,18 @@ function ProductCard({ product }) {
   let capitalizedCategory = product.category.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
   return (
     <div className="product-card">
-      <img src={product.image} alt={product.title} className="product-image" />
-      <p className="product-category">{capitalizedCategory}</p>
-      <p className="product-title">{product.title}</p>
-      <div className="product-price-div">
-        <p className="product-price">${product.price}</p>
-        <button className="add-to-cart">Add to cart</button>
+      <div className="product-img-div">
+        <img src={product.image} alt={product.title} className="product-image" />
+      </div>
+      <div className="product-info-div">
+        <div className="product-text-div">
+          <p className="product-category">{capitalizedCategory}</p>
+          <p className="product-title">{product.title}</p>
+        </div>
+        <div className="product-price-div">
+          <p className="product-price">${product.price}</p>
+          <button className="add-to-cart">Add to cart</button>
+        </div>
       </div>
     </div>
   );
